ecbe3d61829d1f3bc45282de928e134c0bc3a015
emma-hedeager <60857267+emma-hedeager@users.noreply.github.com>
2020-10-27 14:41:44 +0100

    Opdateret plugin

--- wp-content/plugins/thplugin/js/script.js
+++ wp-content/plugins/thplugin/js/script.js
@@ -9,16 +9,3 @@ jQuery(document).ready(function ($) {
         $('.vejledning-form').hide();
     })
 });
-
-function erSynlig(elementClass) {
-    const elementBoks = document.getElementByClass(vejledning-form).getBoundingClientRect();
-    const halvtredsPct = elementBoks.height * 0.5;
-    const start = window.innerHeight - halvtredsPct;
-
-    if (elementBoks.top <= start && elementBoks.bottom - halvtredsPct > 0) {
-        return true;
-    } else {
-        return false;
-    }
-
-}
