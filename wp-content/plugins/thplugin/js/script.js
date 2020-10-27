1fc06d27f820757b9b9670f9967456fd2cb0872d
emma-hedeager <60857267+emma-hedeager@users.noreply.github.com>
2020-10-27 11:20:03 +0100

    Animation til knapper og opdateret plugin

--- wp-content/plugins/thplugin/js/script.js
+++ wp-content/plugins/thplugin/js/script.js
@@ -9,3 +9,16 @@ jQuery(document).ready(function ($) {
         $('.vejledning-form').hide();
     })
 });
+
+function erSynlig(elementClass) {
+    const elementBoks = document.getElementByClass(vejledning-form).getBoundingClientRect();
+    const halvtredsPct = elementBoks.height * 0.5;
+    const start = window.innerHeight - halvtredsPct;
+
+    if (elementBoks.top <= start && elementBoks.bottom - halvtredsPct > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
